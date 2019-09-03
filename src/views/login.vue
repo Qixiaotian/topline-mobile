f95f7f05676b2aa108f03a76f91decb510720e53
qixiaotian <15148192179@163.com>
2019-09-03 22:20:35 +0800

    登录请求封装方法,发送请求,设置失败成功的提示

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,26 +2,53 @@
   <div>
     <van-nav-bar title="登录" />
     <van-cell-group>
-      <van-field  left-icon="phone" right-icon="question-o" placeholder="请输入手机号" />
-      <van-field   placeholder="请输入手机号" left-icon="lock">
-         <van-button slot="button" size="small" type="default" class="send-btn">发送验证码</van-button>
+      <van-field
+        v-model="user.mobile"
+        left-icon="phone"
+        right-icon="question-o"
+        placeholder="请输入手机号"
+      />
+      <van-field v-model="user.code" placeholder="请输入手机号" left-icon="lock">
+        <van-button slot="button" size="small" type="default" class="send-btn">发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <div class="click-btn">
-      <van-button type="info" size="large">登录</van-button>
+      <van-button type="info" size="large" @click="loginFun">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { login } from '../api/user'
+export default {
+  data () {
+    return {
+      user: {
+        mobile: '139111111111',
+        code: '246810'
+      }
+    }
+  },
+  methods: {
+    async loginFun () {
+      try {
+        let res = await login(this.user)
+        // this.$router.push('./home')
+        console.log(res)
+        this.$toast.success('登陆成功')
+      } catch (err) {
+        console.log(err)
+        this.$toast.fail('登陆失败')
+      }
+    }
+  } }
 </script>
 <style lang="less" scoped>
-.send-btn{
-  background-color:#ededed;
-  border-radius: 40px
-}
-  .click-btn{
-     padding: 20px;
+  .send-btn {
+    background-color: #ededed;
+    border-radius: 40px;
+  }
+  .click-btn {
+    padding: 20px;
   }
 </style>
