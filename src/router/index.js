35d2bbecbf9c9e76e4b66278162c8e2ab3e642bd
qixiaotian <15148192179@163.com>
2019-09-03 21:42:45 +0800

    通过vant 创建登录页面 挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '../views/login.vue'
 Vue.use(Router)
-
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    }
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <van-nav-bar title="登录" />
+    <van-cell-group>
+      <van-field  left-icon="phone" right-icon="question-o" placeholder="请输入手机号" />
+      <van-field   placeholder="请输入手机号" left-icon="lock">
+         <van-button slot="button" size="small" type="default" class="send-btn">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="click-btn">
+      <van-button type="info" size="large">登录</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.send-btn{
+  background-color:#ededed;
+  border-radius: 40px
+}
+  .click-btn{
+     padding: 20px;
+  }
+</style>
