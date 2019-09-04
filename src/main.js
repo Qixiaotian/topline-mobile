b0c24daaec421311c232c6c4c5eeda7b7bb9f3ee
qixiaotian <15148192179@163.com>
2019-09-04 14:54:44 +0800

    验证规则

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import './styles/login.less'
+import VeeValidate from 'vee-validate'
 Vue.use(Vant)
+Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login.vue
+++ src/views/login.vue
@@ -3,12 +3,22 @@
     <van-nav-bar title="登录" />
     <van-cell-group>
       <van-field
+        v-validate="'required|digits:11'"
+        name="mobile"
+        :error-message="errors.first('mobile')"
         v-model="user.mobile"
         left-icon="phone"
         right-icon="question-o"
         placeholder="请输入手机号"
       />
-      <van-field v-model="user.code" placeholder="请输入手机号" left-icon="lock">
+      <van-field
+        v-validate="'required||digits:6'"
+        name="code"
+        :error-message="errors.first('code')"
+        v-model="user.code"
+        placeholder="请输入手机号"
+        left-icon="lock"
+      >
         <van-button slot="button" size="small" type="default" class="send-btn">发送验证码</van-button>
       </van-field>
     </van-cell-group>
@@ -20,6 +30,7 @@
 
 <script>
 import { login } from '../api/user'
+import { mapMutations } from 'vuex'
 export default {
   data () {
     return {
@@ -30,19 +41,21 @@ export default {
     }
   },
   methods: {
+    ...mapMutations(['setPush']),
     async loginFun () {
       try {
         let res = await login(this.user)
         // this.$router.push('./home')
-        console.log(res)
-        this.$store.commit('setPush', res)
+        // this.$store.commit('setPush', res)
+        this.setPush(res)
         this.$toast.success('登陆成功')
       } catch (err) {
         console.log(err)
         this.$toast.fail('登陆失败')
       }
     }
-  } }
+  }
+}
 </script>
 <style lang="less" scoped>
   .send-btn {
