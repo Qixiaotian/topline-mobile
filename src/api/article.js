8a5360f06f04b96180a8eac0c38c5e9bdcda592c
qixiaotian <15148192179@163.com>
2019-09-07 11:12:47 +0800

    举报文及优化方案

--- src/api/article.js
+++ src/api/article.js
@@ -10,8 +10,13 @@ export const getArticles = ({
 }) => {
   return request.get('/app/v1_1/articles', { params: { channel_id, timestamp, with_top } })
 }
+// 封装不感兴趣的文章的api
 export const dislikeArticle = (id) => {
   return request.post('/app/v1_0/article/dislikes', {
     target: id
   })
+}
+// 封装举报文章的api
+export const reports = ({ target, type }) => {
+  return request.post('/app/v1_0/article/reports', { target, type })
 }
--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -14,16 +14,15 @@
     </van-cell-group>
     <!-- 举报文章 -->
     <van-cell-group v-show="showReports">
-      <van-cell icon="arrow-left" @click="showReports=false" />
-      <van-cell title="标题夸张" />
-      <van-cell title="低俗色情" />
+      <van-cell icon="arrow-left" @click="showReports=false"></van-cell>
+      <van-cell v-for="item in typeList" :key="item.type" :title="item.title" @click="handle('report',item.type)" />
       <van-cell title="错别字多" />
     </van-cell-group>
   </van-dialog>
 </template>
 
 <script>
-import { dislikeArticle } from '@/api/article'
+import { dislikeArticle, reports } from '@/api/article'
 import { blacklists } from '@/api/user'
 export default {
   name: 'MoreAction',
@@ -40,14 +39,26 @@ export default {
   data () {
     return {
       sha: true,
-      showReports: false
+      showReports: false,
+      // 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-低俗色情-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权'
+      typeList: [
+        { title: '其他问题', type: 0 },
+        { title: '标题夸张', type: 1 },
+        { title: '低俗色情', type: 2 },
+        { title: '低俗色情', type: 3 },
+        { title: '旧闻重复', type: 4 },
+        { title: '广告软文', type: 5 },
+        { title: '内容不实', type: 6 },
+        { title: '涉嫌违法犯罪', type: 7 },
+        { title: '侵权', type: 8 }
+      ]
     }
   },
   created () {
     // console.log(this.article)
   },
   methods: {
-    handle (type) {
+    handle (type, reportType) {
       switch (type) {
         case 'dislike':
           this.dislike()
@@ -55,9 +66,12 @@ export default {
         case 'blacklist':
           this.blacklist()
           break
+        case 'report':
+          this.report(reportType)
+          break
       }
     },
-    async  dislike () {
+    async dislike () {
       try {
         await dislikeArticle(this.article.art_id)
         this.$toast.success('操作成功')
@@ -73,6 +87,18 @@ export default {
         this.$emit('handlesuccess')
       } catch (err) {
         this.$toast.fail('操作失败')
+      }
+    },
+    async report (reportType) {
+      try {
+        await reports({ target: this.article.art_id, type: reportType })
+        this.$emit('input', false)
+        this.$toast.success('操作成功')
+        this.showReports = false
+      } catch (err) {
+        // console.log(err)
+        this.$toast.fail('操作失败')
+        this.showReports = false
       }
     }
   }
