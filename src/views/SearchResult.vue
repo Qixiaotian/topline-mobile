f6bb19573a4a69afd4e014a9eadf1e81234e9eb4
qixiaotian <15148192179@163.com>
2019-09-08 22:24:51 +0800

    搭载列表组件 vant里面list---cell

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -5,20 +5,57 @@
     title="搜索结果"
     left-text="返回"
     left-arrow
-    @click-left="$router.back()"
+    fixed
+   @click-left="$router.back()"
    />
-    这是啥呀
-   {{q}}
+   <van-list
+      v-model="loading"
+      :finished="finished"
+       finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchResult',
-  props: ['q']
+  props: ['q'],
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
+
 </script>
 
-<style>
-
+<style lang="less" >
+.van-list {
+  margin-top: 46px;
+}
 </style>
