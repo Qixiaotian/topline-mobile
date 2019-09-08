d450e3e3324b2ba54f9f06c1665a0142f71404ed
qixiaotian <15148192179@163.com>
2019-09-08 20:44:36 +0800

    历史删除消息 本地存储 历史显示

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,29 +5,36 @@
       v-model="value"
       placeholder="请输入搜索关键词"
       show-action
-      @search="onSearch"
+      @search="onSearch(value)"
       @cancel="onCancel"
       @input="handleInput"
       background="#3e9df8"
     />
     <!-- 搜索提示 -->
     <van-cell-group v-show="value">
-      <van-cell v-for="item in suggestionList" :key="item" :title="item" icon="search"/>
+      <van-cell
+        v-for="item in suggestionList"
+        :key="item"
+        :title="item"
+        icon="search"
+        @click="onSearch(item)"
+      />
     </van-cell-group>
 
     <!-- 历史记录 -->
     <van-cell-group v-show="!value">
       <van-cell title="历史记录">
         <!-- 自定义右侧内容 -->
-        <div>
+        <div v-show="isEdit" style="display: inline-block">
+        <!-- <div v-show="isEdit" style="display:inline-block "> -->
           <span>全部删除</span>&nbsp;
-          <span>完成</span>&nbsp;
-          <van-icon name="delete" size="18px" />
+          <span @click="isEdit=false" >完成</span>&nbsp;
         </div>
+          <van-icon @click="isEdit=true" name="delete" size="18px" v-show="!isEdit" />
       </van-cell>
-      <van-cell title="单元格">
+      <van-cell  v-for="(item,index) in histories" :key="item" :title="item">
         <!-- 自定义右侧内容 -->
-        <van-icon name="close" size="18px" />
+        <van-icon v-show="isEdit" @click="handleDelete(index)" name="close" size="18px" />
       </van-cell>
     </van-cell-group>
   </div>
@@ -35,18 +42,47 @@
 
 <script>
 import { getSuggestion } from '@/api/search'
+import { mapState } from 'vuex'
+import * as storageTools from '@/utils/localStorage'
 export default {
   name: 'Search',
   data () {
     return {
       value: '',
-      suggestionList: []
+      suggestionList: [],
+      isEdit: false,
+      histories: []
     }
   },
+  computed: {
+    ...mapState(['user'])
+  },
+  created () {
+    if (this.user) {
+      return
+    }
+    this.histories = storageTools.getItem('history') || []
+  },
   methods: {
-    onSearch () {},
+    onSearch (item) {
+      if (this.histories.includes(item)) {
+        // this.histories = set
+        // this.histories.unshift(set)
+        return
+      }
+      this.histories.unshift(item)
+      // var set = new Set(this.histories)
+      if (this.user) {
+        return
+      }
+      storageTools.setItem('history', this.histories)
+    },
     onCancel () {},
-    async  handleInput () {
+    handleDelete (index) {
+      this.histories.splice(index, 1)
+      storageTools.setItem('history', this.histories)
+    },
+    async handleInput () {
       if (!this.value) {
         return
       }
