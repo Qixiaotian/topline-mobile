efaab0259ed8d910726b5aa2eb5fcdbf7df5a8d8
qixiaotian <15148192179@163.com>
2019-09-10 14:28:23 +0800

    对refresh_token进行实现------->登录成功返回原来的页面

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import bigint from 'json-bigint'
 import store from '@/store/index'
+import router from '@/router'
 // 创建一个实例
 export const createAPI = (url, method, data) => {
   let config = {}
@@ -40,7 +41,36 @@ instance.interceptors.request.use(config => {
 // axios设置响应拦截器
 instance.interceptors.response.use(response => {
   return response.data.data || response.data // 拦截处理响应结果，直接返回需要的数据
-}, err => {
-  console.log(err)
+}, async function (err) {
+  if (err.response.status === 401) {
+    // 如果是401，使用refresh_token交换新的token
+    const refreshToken = store.state.user.refresh_token
+    try {
+      const response = await axios({
+        method: 'put',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${refreshToken}`
+        }
+      })
+      // 将请求回来的token,和refresh_token放到仓库里面
+      const token = response.data.data.token
+      store.commit('setUer', {
+        token: token,
+        refresh_token: refreshToken
+      })
+      //  在内部自动进行发送请求  其中的参数都在config对象里面
+      return instance(err.config)
+    } catch (err) {
+      console.log(err)
+      router.push({
+        path: '/login',
+        query: {
+          redirect: router.currentRoute.fullPath
+        }
+      })
+    }
+  }
+  return Promise.reject(err)
 })
 export default instance
