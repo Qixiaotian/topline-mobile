d908a163850bf8fbe252db0e974da3d2d0ec7b0b
qixiaotian <15148192179@163.com>
2019-09-13 01:06:40 +0800

    图片预览功能的实现

--- src/views/user-profile/componenct/UploadFile.vue
+++ src/views/user-profile/componenct/UploadFile.vue
@@ -6,7 +6,9 @@
     closeOnClickOverlay
   >
     <van-cell-group>
-      <van-cell title="从相册选择图片" />
+      <van-cell title="从相册选择图片" @click="handleSelectFile" />
+      <!-- 添加一个上传文件的input文本框,隐藏 -->
+      <input type="file" ref="file" style="display:none">
       <van-cell title="拍照" />
       <van-cell title="取消" @click="$emit('input', false)" />
     </van-cell-group>
@@ -14,9 +16,39 @@
 </template>
 
 <script>
+import Vue from 'vue'
+// 使用vant的预览方法
+import { ImagePreview } from 'vant'
+Vue.use(ImagePreview)
 export default {
   name: 'UploadFile',
-  props: ['value']
+  props: ['value'],
+  methods: {
+    // 通过点击方法 触发点击文件的上传事件
+    handleSelectFile () {
+      this.$refs.file.click()
+      // 如果没有上传文件将会返回
+      this.$refs.file.onchange = (e) => {
+        if (e.target.files.length === 0) {
+          return
+        }
+        // 找到图片内存中找到可以访问的临时地址
+        const url = URL.createObjectURL(e.target.files[0])
+        //  将通知父组件进行关闭弹框
+        this.$emit('input', false)
+        // 图片预览
+        ImagePreview({
+          images: [
+            url
+          ],
+          showIndex: false,
+          onClose () {
+
+          }
+        })
+      }
+    }
+  }
 }
 </script>
 
