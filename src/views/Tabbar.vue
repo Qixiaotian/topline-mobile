1216ade0c0aeb6415d4110ae84bbe3efac08401a
qixiaotian <15148192179@163.com>
2019-09-12 11:58:21 +0800

    显示登陆页面--->根据user显示登陆的状态还是未登录的页面

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -6,6 +6,7 @@
       <van-tabbar-item icon="home-o" to="/">主页</van-tabbar-item>
       <van-tabbar-item icon="chat" to="qa">问答</van-tabbar-item>
       <van-tabbar-item icon="play-circle" to="video">视频</van-tabbar-item>
+      <!-- 通过在仓库的存储的user判断是否存在显示我的还是未登录状态 -->
       <van-tabbar-item icon="manager" to="/user" >{{user?'我的':'未登录'}}</van-tabbar-item>
     </van-tabbar>
   </div>
